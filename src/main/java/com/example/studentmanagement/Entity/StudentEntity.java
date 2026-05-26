371952ba3a2ceab659fa6fd4b2f61fc435a9d4a0
amit pratap singh <amitrajvi773@gmail.com>
2026-05-27 00:33:19 +0530

    add new maven dependency

--- src/main/java/com/example/studentmanagement/Entity/StudentEntity.java
+++ src/main/java/com/example/studentmanagement/Entity/StudentEntity.java
@@ -1,4 +1,12 @@
 package com.example.studentmanagement.Entity;
 
+import jdk.jfr.DataAmount;
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Data
+@Entity
 public class StudentEntity {
+    private String studentname;
 }
