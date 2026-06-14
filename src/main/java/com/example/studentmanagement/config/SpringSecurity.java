33ee18ffceae12bd8ea0a95b6f38e6edda193a55
amit pratap singh <amitrajvi773@gmail.com>
2026-06-15 01:05:50 +0530

    update

--- src/main/java/com/example/studentmanagement/config/SpringSecurity.java
+++ src/main/java/com/example/studentmanagement/config/SpringSecurity.java
@@ -1,5 +1,33 @@
 package com.example.studentmanagement.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.authentication.PasswordEncoderParser;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurity {
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth->auth.requestMatchers("/public","/public/**").permitAll().
+                requestMatchers("/student","/student/**").authenticated().
+                requestMatchers("/school","/school/**").permitAll().
+                requestMatchers("/admin/**","/ADMIN/**").authenticated()
+                .anyRequest().authenticated())
+                 .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
