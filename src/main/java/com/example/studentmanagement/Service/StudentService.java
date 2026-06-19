2c59bdb280d6ab79c908929e8d996ae26a97c08a
amit pratap singh <amitrajvi773@gmail.com>
2026-06-19 23:16:52 +0530

    update

--- src/main/java/com/example/studentmanagement/Service/StudentService.java
+++ src/main/java/com/example/studentmanagement/Service/StudentService.java
@@ -4,6 +4,7 @@ import com.example.studentmanagement.Entity.SchoolEntity;
 import com.example.studentmanagement.Entity.StudentEntity;
 import com.example.studentmanagement.Repository.SchoolRepository;
 import com.example.studentmanagement.Repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Service
 public class StudentService {
 @Autowired
@@ -34,7 +35,7 @@ public void saveStudent(StudentEntity entitydata, String schoolname){
     SchoolEntity school=schoolService.findbyschoolname(schoolname);
    StudentEntity savestudent= studentRepository.save(entitydata);
    try{
-       if(savestudent!=null){
+       if(savestudent != null){
    school.getStudentEntries().add(savestudent);
    schoolService.saveSchool(school);}}
    catch (Exception e) {
@@ -48,15 +49,26 @@ public Optional<StudentEntity> findStudentById(@PathVariable Integer id){
     return studentRepository.findById(id);
 
 }
-    public Optional<StudentEntity> findStudentByName(@PathVariable String username){
+public Optional<StudentEntity> findStudentByName(@PathVariable String username){
 
         return studentRepository.findByStudentname(username);
 
     }
 
-    public void deleteStundent(@PathVariable Integer id) {
 
-    studentRepository.deleteById(id);
+    public void deleteStundent(@PathVariable Integer id,String schoolname) {
+    try {
+        SchoolEntity stud=schoolService.findbyschoolname(schoolname);
+        boolean removed=stud.getStudentEntries().removeIf(x->x.getId().equals(id));
+        if(removed){
+              schoolRepository.save(stud);
+              studentRepository.deleteById(id);
+                  }
+              }
+    catch (Exception e) {
+        log.error("this is error in deletion service ",e);
+    }
+
 
     }
 
